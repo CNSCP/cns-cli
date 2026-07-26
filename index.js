ca9a6042267940a5e0c099880167526153306ac2
Andrew <mindovermiles262@gmail>
2026-07-26 15:05:47 -0700

    fix: do not load websockets
    Signed-off-by: Andrew <mindovermiles262@gmail>

--- index.js
+++ index.js
@@ -2470,8 +2470,7 @@ function verifyToken(header, done) {
 
 // Authorize dashboard HTTP request
 function authorize(req, res, next) {
-  // Always allow the dashboard page and its top-level assets
-  if (req.method === 'GET' && /^\/([^/]+\.(js|css))?$/.test(req.path)) {
+  if (!req.ws) {
     next();
     return;
   }
